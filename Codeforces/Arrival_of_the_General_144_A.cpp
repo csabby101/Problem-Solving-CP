0b9b7555c7d0a4b92f45e40a435f671aca97c870
Sarthak Bindal <sarthak2132006@gmail.com>
2021-07-27 23:24:50 +0530

    vector approach using rev iterators

--- Codeforces/Arrival_of_the_General_144_A.cpp
+++ Codeforces/Arrival_of_the_General_144_A.cpp
@@ -1,15 +1,20 @@
+// https://codeforces.com/problemset/problem/144/A
+// complicated vector approach using reverse iterators 
+// and converting reverse iterator to nomal iterator using .base() function at line 25 
+// For reference : https://stackoverflow.com/questions/2037867/can-i-convert-a-reverse-iterator-to-a-forward-iterator
+
 #include <bits/stdc++.h>
 using namespace std;
-
+ 
 int main()
 {
 	ios::sync_with_stdio(false); cin.tie(NULL);
-
+ 
 	int n;
 	cin >> n;
-
+ 
 	vector<int> v(n);
-
+ 
 	for (int i = 0; i < n; i++)
 	{
 		int x;
@@ -17,25 +22,16 @@ int main()
 		v[i] = x;
 	}
 	int steps = 0;
-	auto min = v.begin();
-	for (auto it = v.begin(); it != v.end(); it++)
-	{
-		if (*it <= *min)
-			min = it;
-	}
-
-	for (auto it = min; it != --v.end(); it++)
+	auto minn = (min_element(v.rbegin(), v.rend()) + 1).base();
+ 
+	for (auto it = minn; it != --v.end(); it++)
 	{
 		swap(*it, *(it + 1));
 		steps++;
 	}
-
-	// for (auto &val : v)
-	// {
-	// 	cout << val << ' '; 
-	// }
+ 
 	auto max = max_element(v.begin(), v.end());
 	steps += max - v.begin();
-
+ 
 	cout << steps;
 }
