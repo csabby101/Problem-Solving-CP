4d031fa59a1a006da492b0fd4332d5150cb5a9e2
Sarthak Bindal <sarthak2132006@gmail.com>
2022-01-19 14:19:06 +0530

    added solution

---
+++ Codeforces/Inna_and_Pink_Pony_374_A.cpp
@@ -0,0 +1,143 @@
+// problem link : https://codeforces.com/contest/374/problem/A
+
+#include <bits/stdc++.h>
+using namespace std;
+using namespace chrono;
+
+//#include <ext/pb_ds/assoc_container.hpp>
+//#include <ext/pb_ds/tree_policy.hpp>
+//using namespace __gnu_pbds;
+
+//typedef tree<int, null_type, less<int>, rb_tree_tag, tree_order_statistics_node_update> ordered_set;
+
+#define endl "\n"
+#define MOD 1000000007
+#define F first
+#define S second
+#define PB push_back
+#define PPB pop_back
+#define MP make_pair
+#define set_bits __builtin_popcountll
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef long double lld;
+
+#ifndef ONLINE_JUDGE
+#define debug(x) cerr << #x << " : "; _print(x); cerr << endl;
+#else
+#define debug(x);
+#endif
+
+void _print(ll t) {cerr << t;}
+void _print(int t) {cerr << t;}
+void _print(float t) {cerr << t;}
+void _print(string t) {cerr << t;}
+void _print(char t) {cerr << t;}
+void _print(lld t) {cerr << t;}
+void _print(double t) {cerr << t;}
+void _print(ull t) {cerr << t;}
+void _print(bool t) {cerr << (t ? "true" : "false");}
+
+template <class T, class V> void _print(pair <T, V> p);
+template <class T> void _print(vector <T> v);
+template <class T> void _print(set <T> v); template <class T> void _print(unordered_set <T> v);
+template <class T, class V> void _print(map <T, V> v); template <class T, class V> void _print(unordered_map <T, V> v);
+template <class T> void _print(multiset <T> v);
+template <class T, class V> void _print(pair <T, V> p) {cerr << "{"; _print(p.F); cerr << ","; _print(p.S); cerr << "}";}
+template <class T> void _print(vector <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T> void _print(set <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T> void _print(unordered_set <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T> void _print(multiset <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T, class V> void _print(map <T, V> v) {cerr << "[ "; for (auto i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T, class V> void _print(unordered_map <T, V> v) {cerr << "[ "; for (auto i : v) {_print(i); cerr << " ";} cerr << "]";}
+// template <class T> void _print(ordered_set v) {cerr << "[ "; for (auto i : v) {_print(i); cerr << " ";} cerr << "]";}
+/*___________________________________________________________________________________________________________________________*/
+/*---------------------------------------------------------------------------------------------------------------------------*/
+
+
+void sarthak()
+{
+    int n, m, i, j, a, b;
+    cin >> n >> m >> i >> j >> a >> b;
+
+    if ((i == 1 || i == n) && (j == 1 || j == m)) 
+    {
+        cout << 0;
+        return;
+    }
+
+    if ((i + a > n) && (i - a < 1) || (j + b > m) && (j - b < 1))
+    {
+        cout << "Poor Inna and pony!";
+        return;
+    }
+
+    vector<pair<int, int> > vc = {MP(1, m), MP(n, 1), MP(n, m), MP(1, 1)};
+
+    int badpair = 0;
+    vector<pair<int, int> > steps;
+
+    for (int k = 0; k < 4; k++)
+    {
+        int diff_i = (vc[k].F - i), diff_j = (vc[k].S - j);
+
+        if (!(diff_i % a) && !(diff_j % b))
+        {
+            int i_s = diff_i / a, j_s = diff_j / b;
+            if ((i_s + j_s) % 2 == 0)
+                steps.PB(MP(i_s, j_s));
+            else
+                badpair++;
+        }
+        else
+            badpair++;
+    }
+
+    // debug(steps);
+    // debug(badpair);
+    
+    if (badpair == 4)
+    {
+        cout << "Poor Inna and pony!";
+        return;
+    }
+
+    int minsteps = INT_MAX;
+
+    for (auto &p : steps)
+    {
+        minsteps = min(minsteps, max(abs(p.F), abs(p.S)));
+    }
+    cout << minsteps;
+}
+
+/*___________________________________________________________________________________________________________________________*/
+/*---------------------------------------------------------------------------------------------------------------------------*/
+int main()
+{
+#ifndef ONLINE_JUDGE
+    freopen("zerror.in", "w", stderr);
+#endif
+
+    ios::sync_with_stdio(false); cin.tie(NULL);
+    auto start = high_resolution_clock::now();
+    /*___________________________________________________________________________________________________________________________*/
+
+    // int t;
+    // cin >> t;
+
+    // while (t--)
+    // {
+    //     sarthak();
+    // }
+
+    sarthak();
+
+    /*____________________________________________________________________________________________________________________________*/
+    auto stop = high_resolution_clock::now();
+    auto duration = duration_cast<microseconds>(stop - start);
+#ifndef ONLINE_JUDGE
+    cerr << "\n-----------------" << "\nTime : " << duration.count() / 1000000.0000 << " s" << endl;
+#endif
+}
