808a25b8ba778a7f68b2ad205a3f97cba011b0fd
Sarthak Bindal <sarthak2132006@gmail.com>
2022-02-01 22:53:15 +0530

    added CF Hello 2022 solutions

---
+++ Codeforces/Contests/Hello 2022/1621_B_Integers_Shop.cpp
@@ -0,0 +1,128 @@
+// problem link : https://codeforces.com/contest/1621/problem/B
+
+#include <bits/stdc++.h>
+using namespace std;
+using namespace chrono;
+
+//#include <ext/pb_ds/assoc_container.hpp>
+//#include <ext/pb_ds/tree_policy.hpp>
+//using namespace __gnu_pbds;
+
+//typedef tree<int, null_type, less<int>, rb_tree_tag, tree_order_statistics_node_update> ordered_set;
+
+#define endl "\n"
+#define MOD 1000000007
+#define F first
+#define S second
+#define PB push_back
+#define PPB pop_back
+#define MP make_pair
+#define set_bits __builtin_popcountll
+#define REP(a) for (int i = 0; i < a; i++)
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef long double lld;
+
+#ifndef ONLINE_JUDGE
+#define debug(x) cerr << #x << " : "; _print(x); cerr << endl;
+#else
+#define debug(x);
+#endif
+
+void _print(ll t) {cerr << t;}
+void _print(int t) {cerr << t;}
+void _print(float t) {cerr << t;}
+void _print(string t) {cerr << t;}
+void _print(char t) {cerr << t;}
+void _print(lld t) {cerr << t;}
+void _print(double t) {cerr << t;}
+void _print(ull t) {cerr << t;}
+void _print(bool t) {cerr << (t ? "true" : "false");}
+
+template <class T, class V> void _print(pair <T, V> p);
+template <class T> void _print(vector <T> v);
+template <class T> void _print(set <T> v); template <class T> void _print(unordered_set <T> v);
+template <class T, class V> void _print(map <T, V> v); template <class T, class V> void _print(unordered_map <T, V> v);
+template <class T> void _print(multiset <T> v);
+template <class T, class V> void _print(pair <T, V> p) {cerr << "{"; _print(p.F); cerr << ","; _print(p.S); cerr << "}";}
+template <class T> void _print(vector <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T> void _print(set <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T> void _print(unordered_set <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T> void _print(multiset <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T, class V> void _print(map <T, V> v) {cerr << "[ "; for (auto i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T, class V> void _print(unordered_map <T, V> v) {cerr << "[ "; for (auto i : v) {_print(i); cerr << " ";} cerr << "]";}
+// template <class T> void _print(ordered_set v) {cerr << "[ "; for (auto i : v) {_print(i); cerr << " ";} cerr << "]";}
+/*___________________________________________________________________________________________________________________________*/
+/*---------------------------------------------------------------------------------------------------------------------------*/
+
+
+void sarthak()
+{
+    int n;
+    cin >> n;
+
+    int l, r, c;
+
+    int l_min = INT_MAX, r_max = 0, cost_l = INT_MAX, cost_r = INT_MAX, total = INT_MAX;
+
+    for (int i = 0; i < n; i++)
+    {
+        cin >> l >> r >> c;
+
+        if (l < l_min)
+        {
+            l_min = l;
+            cost_l = c;
+            total = INT_MAX;
+        }
+
+        else if (l == l_min && c < cost_l)
+            cost_l = c;
+
+        if (r > r_max)
+        {
+            r_max = r;
+            cost_r = c;
+            total = INT_MAX;
+        }
+
+        else if (r == r_max && c < cost_r)
+            cost_r = c;
+
+        if (l == l_min && r == r_max && c < total)
+            total = c;
+
+        cout << min(total, cost_r + cost_l) << endl;
+    }
+}
+
+/*___________________________________________________________________________________________________________________________*/
+/*---------------------------------------------------------------------------------------------------------------------------*/
+int main()
+{
+#ifndef ONLINE_JUDGE
+    freopen("zerror.in", "w", stderr);
+#endif
+
+    ios::sync_with_stdio(false); cin.tie(NULL);
+    auto start = high_resolution_clock::now();
+    /*___________________________________________________________________________________________________________________________*/
+
+    int t;
+    cin >> t;
+
+    while (t--)
+    {
+        sarthak();
+    }
+
+    //sarthak();
+
+    /*____________________________________________________________________________________________________________________________*/
+    auto stop = high_resolution_clock::now();
+    auto duration = duration_cast<microseconds>(stop - start);
+#ifndef ONLINE_JUDGE
+    cerr << "\n-----------------" << "\nTime : " << duration.count() / 1000000.0000 << " s" << endl;
+#endif
+}
